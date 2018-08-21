53987a395c37c30a14f30d09faa246864b3d8acb
ZenRay <jake2011ren@gmail.com>
2018-08-21 15:47:32 +0800

    feat($preprocess): add data information
    add new values into the new field
    
    fixes #1

--- tool/preprocess.py
+++ tool/preprocess.py
@@ -46,3 +46,36 @@ def extract_data(data, columns):
 		print(e)
 
 	return result
+
+def add_single(data, new_values:dict):
+	""" Add information to the data contained columns
+	add new values that stores in new columns, which can be used to
+	analysis data in the next step
+
+  	Parameters:
+		data : pandas DataFrame
+			An Original data is readed from pandas that is a raw information
+
+		new_values : dict
+			column lables stores in the keys, which contains single value
+	
+	Results:
+		result : pandas DataFrame
+			An information data contains the new fields
+
+	Examples
+	--------
+	>>> df = pd.DataFrame(np.random.randint(low=0, high=10, size=(5, 5)),
+	...                    columns=['a', 'b', 'c', 'd', 'e'])
+	>>> add_single(df, {'c':5})
+	    a   b	c
+	0   2   8	5 
+	1   4   2 	5
+	2   1   0 	5
+	3   5   1 	5
+	4   6   0  	5
+	"""
+	result = data.copy()
+	result[new_values.keys()[0]] = new_values.values()[0]
+
+	return result
