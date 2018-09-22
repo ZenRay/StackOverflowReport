4c85a270e1dd343d940565d0f9c409bd5f0b5f1a
ZenRay <jake2011ren@gmail.com>
2018-09-23 02:39:34 +0800

    feat($navbar): layout about the navbar
    the layout about the navbar is done
    
    fixes #6

--- report/DataAnalysis/src/components/NavBar.vue
+++ report/DataAnalysis/src/components/NavBar.vue
@@ -14,6 +14,44 @@
       </div>
       <a href="."><div class="header-logo"></div></a>
     </div>
+
+    <div class="block-splash" id="top">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="splash-content">
+              <div class="splash-logo hidden-xs"></div>
+              <h1 class="_white">
+                StackOverFlow Survey Analysis
+              </h1>
+              <h1 class="_white _bold _line-height-small">
+                Between 2017 And 2018
+              </h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="block-wrapper row">
+        <div class="col-md-3 col-sm-4" id="sidebar">
+          <div class="side-nav hidden-xs" data-offset-top="422" data-spy="affix">
+            <div class="logo-nav"></div>
+            <ul class="nav main-nav">
+              <li v-for="item in navlist" :key="item.id">
+                <a v-bind="{ href: '#'+item}">{{item}}</a>
+              </li>
+            </ul>
+            <ul class="back-to-top _muted">
+              <li>
+                <a href="#top">Back to top<span class="icon"></span></a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
