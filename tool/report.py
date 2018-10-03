a2cb33df5764758d6c7146326472b2f65e913163
ZenRay <jake2011ren@gmail.com>
2018-10-03 19:22:43 +0800

    feat($dataanalysis): pie plot arguments
    create the pie plot arguments labels and colors those are parsed from label_count
    
    fixes #7

--- tool/report.py
+++ tool/report.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+#-*-coding: utf-8 -*-
 """Data Analysis
     purpose: module to analysis the data
 """
@@ -34,11 +36,62 @@ def count_values(data, column, split_option=";"):
             tem = set(i.split(split_option))
             labels |= tem
 
-        for label in tem:
-            if label_count.get(label, 0) == 0:
-                label_count[label] = 1
-            else:
-                label_count[label] +=1
+            for label in tem:
+                if label_count.get(label, 0) == 0:
+                    label_count[label] = 1
+                else:
+                    label_count[label] +=1
 
     return labels, label_count
 
+def bar_plot_option(label_count, top=None, color_option=None, value_option=False):
+    """ Deal with label count
+    deal with the label_count, so that can be used to plot 
+    Parameters:
+        label_count: dict
+            the data is parse from the specific column. Key is label, value is 
+            ammount number
+        top: int default None
+            Specify the top level value, which check the option amount. If it is
+            None, return labels that is a index
+        color_option: list default None
+            A color list will be used to convert the label amount. Caution last
+            element is default color
+        value_option: boolean default False
+            If True, concate the value formated to labels
+    
+    Result:
+        labels: ndarray or list
+            It is used to plot that is a argument labels
+        colors: list optional value
+            It is used to plot that is a argument colors
+    """
+    series = pd.Series(label_count).sort_values()
+    labels = []
+    colors = []
+    choose_option = 0
+
+    if not top:
+        labels = series.index
+        return labels
+    # top is not None
+    for label, condition in \
+        zip(
+            series.index, 
+            series.apply(lambda x: True if x in series.nlargest(top).values else False)
+        ):
+
+        if condition:
+            colors.append(color_option[choose_option])
+            choose_option += 1
+            # update the label with format value
+            if value_option:
+                value = "\n{:0.2f}%".format(series.loc[label] / series.sum() * 100)
+                labels.append(label + value)
+            else:
+                labels.append(label)
+        else:
+            labels.append("")
+            colors.append(color_option[-1])
+            
+    return labels, colors
